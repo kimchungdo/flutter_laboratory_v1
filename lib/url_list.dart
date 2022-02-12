1637f435484245a4644c9a3038e729f9800a5fcb
Chung-Do Kim <dorian21@naver.com>
2022-02-12 21:10:00 +0900

    Update url_list.dart

--- lib/url_list.dart
+++ lib/url_list.dart
@@ -2,4 +2,3 @@
 
 String video1 = 'https://assets.mixkit.co/videos/preview/mixkit-group-of-friends-partying-happily-4640-large.mp4';
 String video2 = 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
-//String video3 = 'https://youtu.be/rbrIGS2pR7I';
